0f060ff09e2c33f2b5fa39b204ca7f84df9ce5a8
David Cha <david.cha44@qmail.cuny.edu>
2019-10-22 14:33:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(a, b) {
-  var add = [a, ...b]
+  var add = [...a, b]
 }
 
 function destructivelyAddElementToBeginningOfArray(a, b) {
