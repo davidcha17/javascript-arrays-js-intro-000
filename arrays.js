083f5761691cc5357497858f93e871cbd8cc60d0
David Cha <david.cha44@qmail.cuny.edu>
2019-10-22 15:10:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(a, b) {
 }
 
 function removeElementFromBeginningOfArray(a, b) {
-  removeElementFromBeginningOfArray.slice(1)
+  a.slice(1)
   return removeElementFromBeginningOfArray
 }
